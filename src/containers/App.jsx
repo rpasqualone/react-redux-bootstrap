979dc7d6d20a198843006a93d08334dcb6c709da
richardp <richardp@eCreations.net>
2017-06-30 00:17:53 -0700

    Initial commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import '../assets/styles/App.scss';
+import { Button, Container, Header } from 'semantic-ui-react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Header as='h1'>Hello world!</Header>
+          <Button
+            content='Discover docs'
+            href='http://react.semantic-ui.com'
+            icon='github'
+            labelPosition='left'
+          />
+        </Container>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcoe to React</h2>
+        </div>
+        <p className="App-intro">
+          To get strted, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import store, { history } from './store';
+
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>, document.getElementById('root')
+);
+registerServiceWorker();
