3e05aba7604a7e3cfe8e8a94eea22d3d99577417
Richard Pasqualone <richard@gingerbreadshed.com>
2017-07-25 16:32:53 -0700

    Prettier test

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -20,7 +20,7 @@ export default class Main extends Component {
 					<h2>Welcome to React</h2>
 				</div>
 				<p className="App-intro">
-					To get strted, edit <code>src/App.js</code> and save to reload.
+					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
 			</div>
 		);
