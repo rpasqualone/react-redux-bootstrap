1d3263b75b6fab26b08512a0e12d687def434b62
Andrew N <andrew@gingerbreadshed.com>
2017-07-26 10:39:31 -0700

    testingggggggggg

--- par/src/containers/App.jsx
+++ par/src/containers/App.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import store, { history } from "../store";
-import NotFound from "../components/NotFound";
-import { ConnectedRouter } from "react-router-redux";
-import { Route, Switch } from "react-router";
-import { AppContainer } from "react-hot-loader";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store, { history } from '../store';
+import NotFound from '../components/NotFound';
+import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from 'react-router';
+import { AppContainer } from 'react-hot-loader';
 
-import Main from "./Main";
+import Main from './Main';
 
-import "../assets/styles/App.scss";
+import '../assets/styles/App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <AppContainer>
-        <Provider store={store}>
-          <ConnectedRouter history={history}>
-            <Switch>
-              <Route exact path="/" component={Main} />
-              <Route component={NotFound} />
-            </Switch>
-          </ConnectedRouter>
-        </Provider>
-      </AppContainer>
-    );
-  }
+	render() {
+		return (
+			<AppContainer>
+				<Provider store={store}>
+					<ConnectedRouter history={history}>
+						<Switch>
+							<Route exact path="/" component={Main} />
+							<Route component={NotFound} />
+						</Switch>
+					</ConnectedRouter>
+				</Provider>
+			</AppContainer>
+		);
+	}
 }
 
 export default App;
