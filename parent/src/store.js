b260c94fe46b126a755966abab3b382067590ef2
Richard Pasqualone <richard@gingerbreadshed.com>
2017-07-26 00:16:24 -0700

    test

--- parent/src/store.js
+++ parent/src/store.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 	const devToolsExtension = window.devToolsExtension;
 
 	if (typeof devToolsExtension === 'function') {
-		enhancers.push(devToolsExtension());
+		  enhancers.push(devToolsExtension());
 	}
 }
 
